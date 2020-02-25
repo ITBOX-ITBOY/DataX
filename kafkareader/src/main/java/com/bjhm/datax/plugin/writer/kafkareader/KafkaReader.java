f72c957910677f15eaee68839fadbaaf170ac111
ITBOX-ITBOY <892847834@QQ.COM>
2020-02-25 17:05:21 +0800

    修改logger.info的信息

--- kafkareader/src/main/java/com/bjhm/datax/plugin/writer/kafkareader/KafkaReader.java
+++ kafkareader/src/main/java/com/bjhm/datax/plugin/writer/kafkareader/KafkaReader.java
@@ -86,14 +86,13 @@ public class KafkaReader extends Reader {
 //                    record1.addColumn(new LongColumn(record.offset()));
 //                    record1.addColumn(new StringColumn(record.key()));
                     for (String split : splits) {
-                        logger.info(split.toString());
                         record1.addColumn(new StringColumn(split));
                     }
                     recordSender.sendToWriter(record1);
                     logger.info("缓存记录------------"+record1.toString());
                     consumer.commitSync();
                 }
-
+                recordSender.flush();
             }
         }
         @Override
